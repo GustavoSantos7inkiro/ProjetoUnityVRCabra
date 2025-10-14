5e40a0fc45a8a27621f33fa7148dd3c88e4a58ec
Gustavo Santos Ferreira da Costa <gustavoibapen@hotmail.com>
2025-10-14 09:49:50 -0300

    Cena do 2 puzzle organizada, 2 puzzle bem dizer completo

--- Assets/Scripts/Voids.cs
+++ Assets/Scripts/Voids.cs
@@ -22,16 +22,28 @@ public class SlotPuzzle : MonoBehaviour
 
     private void Awake()
     {
-        // Configura a rotação de cada slot conforme o ID da pintura
+        // Configura a rotação e posição de cada slot conforme o ID da pintura
         switch (idSlot)
         {
+            case 12:
+                correctPosition = new Vector3(13.4943848f, 1.9156822f, -6.4223337f);
+                correctRotation = new Quaternion(0.015839254f, 0.72960615f, 0.028200692f, -0.6831023f);
+                break;
             case 13:
                 correctPosition = new Vector3(11.024384f, 1.941682f, -8.654334f);
                 correctRotation = new Quaternion(0.0108339237f, -0.0096537312f, -0.0031061104f, 0.9998899102f);
                 break;
+            case 21:
+                correctPosition = new Vector3(8.554384f, 1.8956822f, -6.5823336f);
+                correctRotation = new Quaternion(-0.008386371f, 0.6983698f, 0.01514516f, 0.7155278f);
+                break;
             case 23:
                 correctPosition = new Vector3(11.054f, 1.976f, -9.133f);
                 correctRotation = new Quaternion(0.007678f, -0.004341f, 0.008497f, 0.999925f);
+                break;
+            case 31:
+                correctPosition = new Vector3(8.1943846f, 1.8756822f, -6.5923338f);
+                correctRotation = new Quaternion(-0.004179882f, 0.7114002f, 0.006714491f, 0.7027427f);
                 break;
             case 32:
                 correctPosition = new Vector3(12.64438438f, 1.945682287f, -6.442334175f);
