f95a289480ed0da021aba47267cda510ec040d9d
Gustavo Santos Ferreira da Costa <gustavoibapen@hotmail.com>
2025-10-14 04:47:29 -0300

    Luz mostra os numeros do codigo do cofre do 1 puzzle agora

--- Assets/Scripts/Disjuntor.cs
+++ Assets/Scripts/Disjuntor.cs
@@ -2,14 +2,20 @@ using UnityEngine;
 
 public class Disjuntor : MonoBehaviour
 {
+    [Header("Configurações dos Fusíveis")]
     public int fusiveisNecessarios = 3;
     private int fusiveisInseridos = 0;
 
+    [Header("Lâmpada que acende quando completo")]
     public Light lampada;
+
+    [Header("Números que aparecem quando a luz acende")]
+    public GameObject[] numeros; // arraste aqui os objetos dos números no Inspector
 
     private void Start()
     {
-        lampada.enabled = false; // come�a apagada
+        lampada.enabled = false; // começa apagada
+        AtualizarVisibilidadeNumeros(false); // números invisíveis no início
     }
 
     public void ContarFusivel()
@@ -19,7 +25,17 @@ public class Disjuntor : MonoBehaviour
         if (fusiveisInseridos >= fusiveisNecessarios)
         {
             lampada.enabled = true;
-            Debug.Log("Todos os fus�veis foram inseridos! L�mpada acesa!");
+            AtualizarVisibilidadeNumeros(true);
+            Debug.Log("Todos os fusíveis foram inseridos! Lâmpada acesa e números revelados!");
+        }
+    }
+
+    private void AtualizarVisibilidadeNumeros(bool visivel)
+    {
+        foreach (GameObject numero in numeros)
+        {
+            if (numero != null)
+                numero.SetActive(visivel);
         }
     }
 }
